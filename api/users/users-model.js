873e172e88dbf7e2bc656ab1b032e03b2193e6f5
John Merz <jmerz826@gmail.com>
2022-01-25 18:20:01 -0500

    db access findById built

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -21,10 +21,10 @@ async function find() {
   const users = await db("users")
     .leftJoin("roles", "users.role_id", "roles.role_id")
     .select("users.user_id", "users.username", "roles.role_id");
-  return users
+  return users;
 }
 
-function findBy(filter) {
+async function findBy(filter) {
   /**
     You will need to join two tables.
     Resolves to an ARRAY with all users that match the filter condition.
@@ -38,9 +38,12 @@ function findBy(filter) {
       }
     ]
    */
+  const users = await db("users")
+    .leftJoin("roles", "users.role_id", "roles.role_id")
+    .select(filter);
 }
 
-function findById(user_id) {
+async function findById(user_id) {
   /**
     You will need to join two tables.
     Resolves to the user with the given user_id.
@@ -51,6 +54,11 @@ function findById(user_id) {
       "role_name": "instructor"
     }
    */
+  const user = await db("users")
+    .where({ user_id })
+    .leftJoin("roles", "users.role_id", "roles.role_id")
+    .select("users.user_id", "users.username", "roles.role_name");
+  return user;
 }
 
 /**
